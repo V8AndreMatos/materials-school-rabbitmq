be0414c3cfa7d16254116cc7bb56c20646b3414a
Andre <andreluisv8@gmail.com>
2025-11-05 15:14:29 -0300

    Created class Material

---
+++ src/main/java/com/materials/school/entity/Material.java
@@ -0,0 +1,61 @@
+package com.materials.school.entity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.Objects;
+
+
+@Entity
+@Table(name = "tb_material")
+@Schema(description = "Entity that represents a school material in the database")
+public class Material {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Schema(description = "User full name", example = "Geography")
+    @NotBlank(message = "name is required")
+    private String name;
+
+    public Material() {
+
+    }
+
+    public Material(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Material material = (Material) o;
+        return Objects.equals(id, material.id);
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
